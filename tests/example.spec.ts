4820ccdd8a77fff2c184af4c169a0eb81ca22759
Barwy <barwy@live.com>
2026-06-29 16:48:44 +0200

    Added checking holiday titles in search results and offers found.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,15 +5,36 @@ await page.goto("https://fakestore.testelka.pl/");
 await page.locator('[class="woocommerce-store-notice__dismiss-link"]').click;
 await page.locator('[id="woocommerce-product-search-field-0"]').fill('windsurfing');
 await page.keyboard.press('Enter');
-//await page.waitForTimeout(3000);
-const productSelector = '[class="woocommerce-loop-product__title"]';
-await page.locator(productSelector);
-const count = await page.locator(productSelector).count();
-console.log(`Found ${count} elements:`);
-for(let i = 0; i < count; i++) {
-    let holidayName: string = await page.locator(productSelector).nth(i).innerText();
-console.log(holidayName);
+await page.waitForTimeout(1500);
+
+const countOffersFound= await page.locator(productLink()).count();
+console.log(`Found ${countOffersFound} product locators:`);
+
+for (let i = 0; i < countOffersFound; i++) {
+  await page.locator(productLink()).scrollIntoViewIfNeeded;
+  let displayedProductTitle: string = await page.locator(productTitle()).nth(i).innerText();
+   console.log(`Title displayed in offers found: ${displayedProductTitle}`);
+  await page.locator(productLink()).nth(i).click();
+  await page.locator(productName()).waitFor({state: 'visible', timeout: 5000});
+  let holidayname: string = await page.locator(productName()).innerText();
+  console.log(`Holiday name in the offer: ${holidayname}`);
+  await page.goBack();
 }
 });
+
+function productTitle() {
+const productSelector = '[class="woocommerce-loop-product__title"]';
+return productSelector;
+}
+
+function productLink() {
+const selectorProductLink = '[class="woocommerce-LoopProduct-link woocommerce-loop-product__link"]';
+return selectorProductLink;
+}
+
+function productName() {
+const productName = '[class="product_title entry-title"]';
+return productName;
+}
 
 //npx playwright test --project chromium --headed
